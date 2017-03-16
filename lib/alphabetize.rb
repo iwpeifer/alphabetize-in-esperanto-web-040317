e9681eddbc12f48cd0ef995ac91bc4196a175929
Isaac Peifer <iwpeifer@gmail.com>
2017-03-16 01:21:15 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
-def alphabetize(arr)
-  # code here
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(array)
+  array.sort_by do |string|
+    string.split("").map do |char|
+      ESPERANTO_ALPHABET.index(char)
+    end
+  end
 end
